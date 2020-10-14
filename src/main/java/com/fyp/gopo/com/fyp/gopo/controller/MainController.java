a2a6b009c64653180769f0687deddbf7e514c43b
Yaser Tirsen <yaser.tirsen@mydit.ie>
2020-10-14 20:18:37 +0100

    created MainController

---
+++ src/main/java/com/fyp/gopo/com/fyp/gopo/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.fyp.gopo.com.fyp.gopo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Gopo!";
+    }
+}
---
+++ src/main/java/com/fyp/gopo/com/fyp/gopo/model/User.java
@@ -0,0 +1,6 @@
+package com.fyp.gopo.com.fyp.gopo.model;
+
+public class User {
+
+
+}
