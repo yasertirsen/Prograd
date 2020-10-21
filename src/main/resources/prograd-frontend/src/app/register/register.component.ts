f3d717304a99bb971c05e49c7197f2c6b33bc2a8
Yaser Tirsen <yaser.tirsen@mydit.ie>
2020-10-21 23:45:51 +0100

    added files

---
+++ src/main/resources/prograd-frontend/src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: StudentModel = {
+    username:'',
+    email:'',
+    password:''
+  };
+  constructor(private client: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  registerStudent(): void {
+    let url = "http://localhost:8082/api/addstudent";
+    this.client.post(url, this.model).subscribe(
+      res => {
+        location.reload();
+      },
+      err => {
+        alert("An error has occurred while registering")
+      }
+    )
+  }
+}
+
+export interface StudentModel {
+  username: string;
+  email: string;
+  password: string;
+}
