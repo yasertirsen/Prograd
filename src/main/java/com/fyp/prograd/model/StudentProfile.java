9e7c79bc8256c0616be4d4a29f77753ca8285f2e
Yacer <mryaser8@gmail.com>
2021-03-21 16:50:06 +0000

    Small changes

--- src/main/java/com/fyp/prograd/model/StudentProfile.java
+++ src/main/java/com/fyp/prograd/model/StudentProfile.java
@@ -20,7 +20,7 @@ public class StudentProfile {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "courseId", referencedColumnName = "courseId")
     private Course course;
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH})
+    @ManyToMany(cascade = CascadeType.ALL)
     private Set<Skill> externalSkills;
     @OneToMany(cascade = CascadeType.ALL)
     private List<Project> projects;
